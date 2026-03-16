4d223f8cc537b60c5937557991c8347d67e92da6
tyrongabriel <51530686+tyrongabriel@users.noreply.github.com>
2026-03-17 00:22:51 +0100

    chore: disabled useless services

--- inventory/services/default.nix
+++ inventory/services/default.nix
@@ -14,9 +14,9 @@ let
     nixos-modules = import ./nixos-modules.nix { inherit inputs; };
     desktop = import ./desktop.nix { inherit inputs; };
     stylix = import ./stylix.nix { inherit inputs; };
-    hytale-server = import ./hytale-server.nix { inherit inputs; };
-    ollama = import ./ollama.nix { inherit inputs; };
-    llama-cpp = import ./llama-cpp.nix { inherit inputs; };
+    #hytale-server = import ./hytale-server.nix { inherit inputs; };
+    #ollama = import ./ollama.nix { inherit inputs; };
+    #llama-cpp = import ./llama-cpp.nix { inherit inputs; };
   };
 in
 lib.foldr lib.recursiveUpdate { } (lib.attrValues services)
